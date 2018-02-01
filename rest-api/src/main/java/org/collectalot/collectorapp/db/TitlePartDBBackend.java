fbda5572949ceaaceebd09b3129bc967c1430426
Jacob Borella <jacobborella@hotmail.com>
2018-02-01 11:28:03 +0100

    solved thread issue

--- rest-api/src/main/java/org/collectalot/collectorapp/db/TitlePartDBBackend.java
+++ rest-api/src/main/java/org/collectalot/collectorapp/db/TitlePartDBBackend.java
@@ -2,7 +2,9 @@ package org.collectalot.collectorapp.db;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 import javax.transaction.Transactional;
 
 import org.collectalot.collectorapp.model.TitlePart;
@@ -15,18 +17,21 @@ public class TitlePartDBBackend {
 //    @Inject
 //    PersistenceHelper helper;
     public TitlePart[] getAllTitleParts(long parentId) {
+    	EntityManager em = emf.createEntityManager();
         return em.createNamedQuery("TitlePart.findAll", TitlePart.class).setParameter("parentId", parentId).getResultList().toArray(new TitlePart[0]);
     }
     public TitlePart getTitlePart(long id) {
+    	EntityManager em = emf.createEntityManager();
     	return em.createNamedQuery("TitlePart.find", TitlePart.class).setParameter("id", id).getSingleResult();
     }
     @Transactional
     public void saveTitlePart(TitlePart tp) {
+    	EntityManager em = emf.createEntityManager();
     	TitlePart tpPersist = em.find(TitlePart.class, tp.getId());
     	tpPersist.setText(tp.getText());
     	em.persist(tpPersist);
     }
-    @PersistenceContext
-    private EntityManager em;
+    @PersistenceUnit
+    private EntityManagerFactory emf;
 
 }
