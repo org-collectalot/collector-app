cb9c2b87eac91da1744bcbe5b3b4111592699381
Jacob Borella <jacobborella@hotmail.com>
2018-02-09 15:27:58 +0100

    removed unused imports

--- rest-api/src/main/java/org/collectalot/collectorapp/db/TitlePartDBBackend.java
+++ rest-api/src/main/java/org/collectalot/collectorapp/db/TitlePartDBBackend.java
@@ -1,9 +1,7 @@
 package org.collectalot.collectorapp.db;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
 import javax.transaction.Transactional;
 
 import org.collectalot.collectorapp.model.TitlePart;
@@ -13,20 +11,17 @@ import org.collectalot.collectorapp.security.UserSession;
 @ApplicationScoped
 public class TitlePartDBBackend {
     public TitlePart[] getAllTitleParts() {
-    	EntityManager em = emf.createEntityManager();
         return em.createNamedQuery("TitlePart.findAllNoParent", TitlePart.class)
         		  .setParameter("uid", UserSession.getUserLoggedOn().getId())
         		  .getResultList().toArray(new TitlePart[0]);
     }
     public TitlePart[] getAllTitleParts(long parentId) {
-    	EntityManager em = emf.createEntityManager();
         return em.createNamedQuery("TitlePart.findAll", TitlePart.class)
         		  .setParameter("parentId", parentId)
         		  .setParameter("uid", UserSession.getUserLoggedOn().getId())
         		  .getResultList().toArray(new TitlePart[0]);
     }
     public TitlePart getTitlePart(long id) {
-    	EntityManager em = emf.createEntityManager();
     	return em.createNamedQuery("TitlePart.find", TitlePart.class)
     		   .setParameter("id", id)
     		   .setParameter("uid", UserSession.getUserLoggedOn().getId())
@@ -34,7 +29,6 @@ public class TitlePartDBBackend {
     }
     @Transactional
     public TitlePart saveTitlePart(TitlePart tp) {
-    	EntityManager em = emf.createEntityManager();
     	tp.setUser(UserSession.getUserLoggedOn());
     	//merge will also check @Version number.
     	//see TitlePart implementation
@@ -42,7 +36,6 @@ public class TitlePartDBBackend {
     }
     @Transactional
     public TitlePart addTitlePart(TitlePart tp) {
-    	EntityManager em = emf.createEntityManager();
     	User userLoggedOn = em.find(User.class, UserSession.getUserLoggedOn().getId());
     	tp.setUser(userLoggedOn);
     	em.persist(tp);
@@ -50,16 +43,12 @@ public class TitlePartDBBackend {
     }
     @Transactional
     public void deleteTitlePart(Long id) {
-    	EntityManager em = emf.createEntityManager();
     	TitlePart tpDelete = em.find(TitlePart.class, id);
     	if(UserSession.getUserLoggedOn().getId() != tpDelete.getUser().getId()) {
     		throw new IllegalAccessError("User not allowed to access this object");
     	}
     	tpDelete.setDeleted(true);
     }
-    @PersistenceUnit
-    private EntityManagerFactory emf;
-//    TODO: Der er sået tvivl om nedenstående er thread safe. Undersøg nærmere.
-//    @PersistenceContext
-//    private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 }
--- rest-api/src/test/java/org/collectalot/collectorapp/model/TitlePartTest.java
+++ rest-api/src/test/java/org/collectalot/collectorapp/model/TitlePartTest.java
@@ -1,7 +1,5 @@
 package org.collectalot.collectorapp.model;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
